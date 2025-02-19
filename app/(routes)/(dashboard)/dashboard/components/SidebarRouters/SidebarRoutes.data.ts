1125e8284db170c847bc25a8cdbc96c75094d27b
Mel Piña <101666856+MelPina@users.noreply.github.com>
2025-02-19 15:48:43 -0400

    fix: Corrigiendo routes

--- app/(routes)/(dashboard)/dashboard/components/SidebarRouters/SidebarRoutes.data.ts
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRouters/SidebarRoutes.data.ts
@@ -22,41 +22,41 @@ export const dataAdminSidebar = [
   {  
     icon: FileScanIcon,  
     label: "Gestión de Vehículos",  
-    href: "dashboard/admin/cars-manager",  
+    href: "/dashboard/admin/cars-manager",  
   },  
   {  
     icon: Calendar,  
     label: "Renta y devoluciones",  
-    href: "dashboard/admin/allreserves",  
+    href: "/dashboard/admin/allreserves",  
   },  
   {  
     icon: BookUserIcon,  
     label: "Gestión de Clientes",  
-    href: "dashboard/admin/clientes",  
+    href: "/dashboard/admin/clientes",  
   },   {  
     icon: UsersIcon,  
     label: "Gestión de Empleados",  
-    href: "dashboard/admin/empleados",  
+    href: "/dashboard/admin/empleados",  
   },   {  
     icon: ShieldQuestionIcon,  
     label: "Inspección de Vehículos",  
-    href: "dashboard/admin/inspeccion",  
+    href: "/dashboard/admin/inspeccion",  
   },   {  
     icon: ComponentIcon,  
     label: "Gestión de Modelos ",  
-    href: "dashboard/admin/modelos",  
+    href: "/dashboard/admin/modelos",  
   },   {  
     icon: FramerIcon,  
     label: "Gestión de Marcas ",  
-    href: "dashboard/admin/marcas",  
+    href: "/dashboard/admin/marcas",  
   },   {  
     icon: CarTaxiFrontIcon,  
     label: "Gestión de Tipos de Vehículos ",  
-    href: "dashboard/admin/tpvehiculos",  
+    href: "/dashboard/admin/tpvehiculos",  
   },   {  
     icon: FuelIcon,  
     label: "Gestión de Tipos de Combustibles ",  
-    href: "dashboard/admin/tpcombustibles",  
+    href: "/dashboard/admin/tpcombustibles",  
   },  
   
   ];
