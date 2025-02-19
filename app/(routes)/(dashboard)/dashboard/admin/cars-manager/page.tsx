312001779997043ee9eb4c3148734810e2ed2b6e
Mel Piña <101666856+MelPina@users.noreply.github.com>
2025-02-19 10:17:10 -0400

    feat: Agregando validaiones de usuario administrador

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -1,10 +1,27 @@
+import { auth } from "@clerk/nextjs/server";
 import { ButtonAddCar } from "./components/ButtonAddCar";
+import { isAdministrator } from "@/lib/isAdministrator";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-export default function CarsManagerPage() {
+export default async function CarsManagerPage() {
+  // const {userId}= auth();
+  // if (!userId || !isAdministrator(userId)) {
+  //   return redirect("/");
+  //   }
+    
+    // const vehiculo = await db.vehiculo.findMany({
+    // where: {
+    // userId,
+    // },
+    // orderBy: {
+    // createdAt: "desc",
+    // },
+    // });
   return (
     <div>
       <div className="flex justify-between">
-        <h2 className="text-2xl font-bold">Manage your cars</h2>
+        <h2 className="text-2xl font-bold">Gestiona tus vehículos</h2>
         <ButtonAddCar/>
       </div>
     </div>
