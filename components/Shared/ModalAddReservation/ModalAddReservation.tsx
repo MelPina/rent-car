02858ea937044788eeec0e10ee78c31cfaa82f54
Mel Piña <101666856+MelPina@users.noreply.github.com>
2025-03-15 11:31:01 -0400

    feat: Agregando diseño para reservas

--- components/Shared/ModalAddReservation/ModalAddReservation.tsx
+++ components/Shared/ModalAddReservation/ModalAddReservation.tsx
@@ -1,55 +1,61 @@
 "use client";
 import { ModalAddReservationProps } from "./ModalAddReservation.types";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Car } from "@prisma/client";
 import { CalendarSelector } from "./CalendarSelector";
 import { addDays } from "date-fns";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { DateRange } from "react-day-picker";
 
 export function ModalAddReservation(props: ModalAddReservationProps) {
-    const { car } = props;
-    const [dateSelected, setDateSelected] = useState<{
-        from: Date | undefined;
-        to: Date | undefined;
-    }>({
-        from: new Date(),
-        to: addDays(new Date(), 5),
-    });
-    const onReserveCar = async (car: Car, dateSelected: any) => { };
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>
-                <Button variant="outline" className="w-full mt-3">
-                    Reservar vehículo
-                </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>
-                        Selecciona las fechas en las quieres alquilar el vehículo
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                    </AlertDialogDescription>
-                       <CalendarSelector setDateSelected ={setDateSelected} carPriceDay={car.priceDay} />
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction onClick={() => onReserveCar(car, "")}>
-                        Reservar vehículo
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    );
+  const { car } = props;
+  const [dateSelected, setDateSelected] = useState<{
+    from: Date | undefined;
+    to: Date | undefined;
+  }>({
+    from: new Date(),
+    to: addDays(new Date(), 5),
+  });
+  const onReserveCar = async (car: Car, dateSelected: DateRange) => {
+    // Lógica para reservar el vehículo
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="w-full mt-3">
+          Reservar vehículo
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Selecciona las fechas en las quieres alquilar el vehículo
+          </AlertDialogTitle>
+          <AlertDialogDescription></AlertDialogDescription>
+        </AlertDialogHeader>
+        <CalendarSelector
+          setDateSelected={setDateSelected}
+          carPriceDay={car.priceDay}
+        />
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onReserveCar(car, dateSelected)}>
+            Reservar vehículo
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
